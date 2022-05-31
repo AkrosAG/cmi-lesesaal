00c3d1798aa796bc6842dbc66807e1d71e5e530f
Benjamin Schäublin <be.schaeublin@gmail.com>
2022-05-31 10:54:31 +0200

    refactor(tests): move scope folder

---
+++ Test/Access/Harvest.Tests/CMIAis/CMIRecordBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMI.Access.Harvest.Tests.CMIAis
+{
+    [TestFixture]
+    public class CMIRecordBuilderTests
+    {
+
+        public void Test()
+        {
+            var sut = new ArchieR
+        }
+    }
+}
