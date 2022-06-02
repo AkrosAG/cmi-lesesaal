306ecd5846cfcd389e5810a826f78cbc842b7390
Ulrich Günther <ulrich.guenther@cmiag.ch>
2022-06-02 10:14:43 +0200

    DisplayProvider  , first attempt

--- CMI/Access/Harvest/ArchiveRecordBuilderFactory.cs
+++ CMI/Access/Harvest/ArchiveRecordBuilderFactory.cs
@@ -9,13 +9,20 @@ namespace CMI.Access.Harvest
         private readonly ApplicationSettings applicationSettings;
         private readonly CachedLookupData cachedLookupData;
         private readonly IArchiveRecordSecurityProvider securityProvider;
+        private readonly IArchiveRecordDisplayProvider displayProvider;
 
-        public ArchiveRecordBuilderFactory(IAISDataProvider aisDataProvider, IArchiveRecordSecurityProvider securityProvider, LanguageSettings languageSettings, ApplicationSettings applicationSettings, CachedLookupData cachedLookupData)
+        public ArchiveRecordBuilderFactory(IAISDataProvider aisDataProvider,
+                                           IArchiveRecordSecurityProvider securityProvider,
+                                           IArchiveRecordDisplayProvider displayProvider,
+                                           LanguageSettings languageSettings,
+                                           ApplicationSettings applicationSettings,
+                                           CachedLookupData cachedLookupData)
         {
             this.aisDataProvider = aisDataProvider;
             this.languageSettings = languageSettings;
             this.applicationSettings = applicationSettings;
             this.securityProvider = securityProvider;
+            this.displayProvider = displayProvider;
             this.cachedLookupData = cachedLookupData;
         }
 
@@ -25,7 +32,7 @@ namespace CMI.Access.Harvest
             switch (Properties.Settings.Default.AisProvider.ToLowerInvariant())
             {
                 case "cmiais":
-                    archiveRecordBuilder = new CMIAIS.CMIAISArchiveRecordBuilder((CMIAIS.CMIAISDataProvider)aisDataProvider, securityProvider, languageSettings);
+                    archiveRecordBuilder = new CMIAIS.CMIAISArchiveRecordBuilder((CMIAIS.CMIAISDataProvider)aisDataProvider, securityProvider, displayProvider, languageSettings);
                     break;
                 case "scopeais":
                     archiveRecordBuilder = new ScopeArchiv.ScopeArchiveRecordBuilder((ScopeArchiv.ScopeAISDataProvider) aisDataProvider, securityProvider, languageSettings, applicationSettings, cachedLookupData);
---
+++ CMI/Access/Harvest/CMIAIS/CMIAISArchiveRecordDisplayProvider.cs
@@ -0,0 +1,19 @@
+﻿using CMI.Contract.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMI.Access.Harvest.CMIAIS
+{
+    public class CMIAISArchiveRecordDisplayProvider : IArchiveRecordDisplayProvider
+    {
+        public async Task<ArchiveRecordDisplay> GetDisplayData(string recordId)
+        {
+            // TODO: Review
+            return await Task.FromResult(new ArchiveRecordDisplay());
+        }
+    }
+}
