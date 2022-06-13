78498d8de3052ece10a94ded5bf9e93fd348a0a7
Ulrich Günther <ulrich.guenther@cmiag.ch>
2022-06-13 18:30:23 +0200

    dynamic script added to indexmanager

---
+++ CMI/Manager/Index/Compiler/IDynamicScript.cs
@@ -0,0 +1,9 @@
+﻿using CMI.Contract.Common;
+
+namespace CMI.Manager.Index.Compiler
+{
+    public interface IDynamicScript
+    {
+        void Execute(ArchiveRecord archiveRecord, ElasticArchiveRecord elasticArchiveRecord);
+    }
+}
