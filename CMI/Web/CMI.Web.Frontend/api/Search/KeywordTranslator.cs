7e6fb44a361aeb90e46afc18833d51d6c00d2419
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-04-26 08:16:42 +0200

    DLS-85 Umsetzung der Zusatzfelder im P-C, Detailsuche

---
+++ CMI/Web/CMI.Web.Frontend/api/Search/KeywordTranslator.cs
@@ -0,0 +1,38 @@
+﻿using CMI.Access.Sql.Lesesaal;
+using CMI.Utilities.Common.Helpers;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMI.Web.Frontend.api.Search
+{
+    public class KeywordTranslator : IFieldTranslator
+    {
+        public QueryContainer CreateQueryForField(SearchField field, UserAccess access)
+        {
+            return new BoolQuery
+            {
+                //Filter = new QueryContainer[]
+                //{
+                //    new TermsQuery
+                //    {
+                //        Field = "primaryDataFulltextAccessTokens",
+                //        Terms = access.CombinedTokens
+                //    }
+                //},
+                Should = new QueryContainer[]
+                {
+                    new WildcardQuery
+                    {
+                        Value = field.Value, // .Escape("formerReferenceCode")
+                        Boost = 1.0,
+                        CaseInsensitive = false,
+                        Field = "referenceCode"
+                    }
+                }
+            };
+        }
+    }
+}
