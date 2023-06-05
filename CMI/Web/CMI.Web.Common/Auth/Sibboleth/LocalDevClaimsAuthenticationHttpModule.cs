bdd13198909102140c766e2cec60f36b093465b0
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-06-05 15:42:20 +0200

    Zwischenstand vor Umbau Zugriffslogik

---
+++ CMI/Web/CMI.Web.Common/Auth/Sibboleth/LocalDevClaimsAuthenticationHttpModule.cs
@@ -0,0 +1,19 @@
+﻿using CMI.Web.Common.Auth.Sibboleth.Types;
+using System.Web;
+
+namespace CMI.Web.Common.Auth.Sibboleth
+{
+    /// <summary>
+    /// Local development claims - must be overridden in each app to manually specify a user/claims
+    /// </summary>
+    public abstract class LocalDevClaimsAuthenticationHttpModule : ShibbolethClaimsAuthenticationHttpModule
+    {
+        protected abstract override ShibbolethAttributeValueCollection GetAttributesFromRequest(HttpRequest request, ShibbolethSessionType sessionType);
+
+        protected override ShibbolethSessionType IsShibbolethSession(HttpRequest request)
+        {
+            // return something other than None
+            return ShibbolethSessionType.Variable;
+        }
+    }
+}
