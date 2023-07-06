933a2fe8dff619cd32d9fee40549a75adcbbf97c
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-07-06 16:14:44 +0200

    Update IndexManager.cs

--- CMI/Manager/Index/IndexManager.cs
+++ CMI/Manager/Index/IndexManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using CMI.Access.Common;
 using CMI.Contract.Common;
 using CMI.Contract.Messaging;
