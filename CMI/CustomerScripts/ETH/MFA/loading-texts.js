f20587e5898fee1c040d8d186f6c3884dd2c4b73
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-09-18 11:47:21 +0200

    Code Review

---
+++ CMI/CustomerScripts/ETH/MFA/loading-texts.js
@@ -0,0 +1,16 @@
+var loadingTexts = {
+  "@metadata": {
+    "authors": [
+      "Evelix GmbH"
+    ],
+    "last-updated": "2023-09-14",
+    "locale": "de"
+  },
+  "lesesaal-header-title": "Virtueller Lesesaal",
+  "lesesaal-text-01": "",
+  "lesesaal-text-02": "Virtueller Lesesaal",
+  "lesesaal-text-03": "",
+  "lesesaal-text-04": "Virtual Reading Room",
+  "cmi-three-dots": "loading-cmi-three-dots",
+  "cmi-three-dots-color": "#215caf"
+}
