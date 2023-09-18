8735cabc56ecf1fa133e4d2b0c39adf999453366
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-09-18 11:47:21 +0200

    Code Review

--- CMI/Web.Clients/web-frontend/src/config/loading-texts.js
+++ CMI/Web.Clients/web-frontend/src/config/loading-texts.js
@@ -1,15 +1,16 @@
 var loadingTexts = {
-  "@metadata": {
-    "authors": [
-      "Evelix GmbH"
-    ],
-    "last-updated": "2023-09-14",
-    "locale": "de"
-  },
-  "lesesaal-header-title": "Hochschularchiv der ETH Zürich",
-  "lesesaal-text-01": "Hochschularchiv der ETH Zürich",
-  "lesesaal-text-02": "Virtueller Lesesaal",
-  "lesesaal-text-03": "ETH Zurich University Archives",
-  "lesesaal-text-04": "Virtual Reading Room",
-  "cmi-three-dots": "cmi-three-dots"
+	"@metadata": {
+		"authors": [
+			"Evelix GmbH"
+		],
+		"last-updated": "2023-09-14",
+		"locale": "de"
+	},
+	"lesesaal-header-title": "Hochschularchiv der ETH Zürich",
+	"lesesaal-text-01": "Hochschularchiv der ETH Zürich",
+	"lesesaal-text-02": "Virtueller Lesesaal",
+	"lesesaal-text-03": "ETH Zurich University Archives",
+	"lesesaal-text-04": "Virtual Reading Room",
+	"cmi-three-dots": "cmi-three-dots",
+	"cmi-three-dots-color": "#B7352D"
 }
