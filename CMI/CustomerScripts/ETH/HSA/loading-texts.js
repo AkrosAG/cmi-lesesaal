1212258441df9f3206541bf72f5fab1f40a0d737
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-09-18 16:07:45 +0200

    MC Clients

--- CMI/CustomerScripts/ETH/HSA/loading-texts.js
+++ CMI/CustomerScripts/ETH/HSA/loading-texts.js
@@ -6,11 +6,14 @@ var loadingTexts = {
     "last-updated": "2023-09-14",
     "locale": "de"
   },
-  "lesesaal-header-title": "Hochschularchiv der ETH Zürich",
+  "lesesaal-header-title": "Management-Client Virtueller Lesesaal",
   "lesesaal-text-01": "Hochschularchiv der ETH Zürich",
   "lesesaal-text-02": "Virtueller Lesesaal <br/>",
   "lesesaal-text-03": "ETH Zurich University Archives",
   "lesesaal-text-04": "Virtual Reading Room",
-  "cmi-three-dots": "cmi-three-dots",
-  "cmi-three-dots-color": "#B7352D"
+  "lesesaal-header-Management-title": "Management-Client Virtueller Lesesaal",
+  "lesesaal-text-Management-01": "Management-Client Virtueller Lesesaal",
+  "lesesaal-text-Management-02": "",
+  "lesesaal-text-Management-03": "",
+  "lesesaal-text-Management-04": ""
 }
