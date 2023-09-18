50b5bb4e9e9e306d4c7996bc3a5e1cf3d39bb380
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-09-18 14:08:44 +0200

    Code Review

--- CMI/CustomerScripts/ETH/TMA/loading-texts.js
+++ CMI/CustomerScripts/ETH/TMA/loading-texts.js
@@ -6,11 +6,10 @@ var loadingTexts = {
     "last-updated": "2023-09-14",
     "locale": "de"
   },
-  "lesesaal-header-title": "Virtueller Lesesaal",
-  "lesesaal-text-01": "",
-  "lesesaal-text-02": "Virtueller Lesesaal",
-  "lesesaal-text-03": "",
-  "lesesaal-text-04": "Virtual Reading Room",
-  "cmi-three-dots": "loading-cmi-three-dots",
-  "cmi-three-dots-color": "#B7352D"
+   "lesesaal-header-title": "Virtueller Lesesaal",
+  "lesesaal-text-01": "Virtueller Lesesaal",
+  "lesesaal-text-02": "",
+  "lesesaal-text-03": "Virtual Reading Room",
+  "lesesaal-text-04": "",
+  "cmi-three-dots": "loading-cmi-three-dots"
 }
