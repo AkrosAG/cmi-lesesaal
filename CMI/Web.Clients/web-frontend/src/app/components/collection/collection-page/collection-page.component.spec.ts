d9b114ec128fa1be49389d36bc658419b82fb9cb
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-09-20 14:47:12 +0200

    Test fix

--- CMI/Web.Clients/web-frontend/src/app/components/collection/collection-page/collection-page.component.spec.ts
+++ CMI/Web.Clients/web-frontend/src/app/components/collection/collection-page/collection-page.component.spec.ts
@@ -64,7 +64,7 @@ describe('auto generate CollectionPageComponent', () => {
 				return this.testTitle;
 			},
 			setTitle(newTitle: string): void {
-				this._siteTitle = this._txt.get('header.title', 'ETH Zurich University Archives');
+				this._siteTitle = this._txt.get('header.title', 'Hochschularchiv der ETH Zürich');
 				let seoTitle = newTitle + ((newTitle && newTitle.length > 0) ? ' - ' : '') + this._siteTitle;
 				this.testTitle = seoTitle;
 			}
