63040e4c6ada621e2860040ecb0b6e5efa029a7a
Ulrich Guenther <uguenther@ensocon.net>
2023-11-07 09:32:37 +0100

    fix format

--- CMI/Web.Clients/web-frontend/src/app/modules/client/components/order/checkoutShippingTypeStep/checkoutShippingTypeStep.component.ts
+++ CMI/Web.Clients/web-frontend/src/app/modules/client/components/order/checkoutShippingTypeStep/checkoutShippingTypeStep.component.ts
@@ -60,8 +60,7 @@ export class CheckoutShippingTypeStepComponent implements OnInit {
 
 		this.isAsOrBvwUser = this._author.isAsUser() || this._author.isEmaUser();
 		
-		let tokens = this._cfg.getSetting('managementClientSettings.orderDigitalUsers','')
-									  .split(';');
+		let tokens = this._cfg.getSetting('managementClientSettings.orderDigitalUsers', '').split(';');
 		this.isDigitalUser = this._author.hasAnyAccessToken(tokens);
 		
 		let activeOrder = this._scs.getActiveOrder();
