8cf538d5010fe1078d96902ab2bb9502f3dc5ad0
PeterEttwein-Evelix <PEttwein@evelix.ch>
2023-11-15 08:01:29 +0100

    CodeReview

--- CMI/Access/Repository/RepositoryDataAccess.cs
+++ CMI/Access/Repository/RepositoryDataAccess.cs
@@ -105,7 +105,7 @@ namespace CMI.Access.Repository
             Log.Information("Getting repository root for package with id {packageId}", packageId);
             var session = connectionFactory.ConnectToFirstRepository();
 
-            // packageId e. g. IE607568
+            // Do we have a document identifier? These have two @ signs in its packageId
             var isDocument = packageId.Split('@').Length == 3;
             if (isDocument)
             {
