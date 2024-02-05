c9f10ffd7aa0d3d5d922ac3f6edf5a89623c6610
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-02-05 17:49:03 +0100

    DLS-338 Mock funktioniert

---
+++ CMI/Manager/Repository/Systems/Mock/MockPackageHandler.cs
@@ -0,0 +1,18 @@
+﻿using CMI.Contract.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CMI.Contract.Common;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CMI.Manager.Repository.Systems.Mock
+{
+    public class MockPackageHandler : IPackageHandler
+    {
+        public Task CreateMetadataXml(string folderName, RepositoryPackage package, List<RepositoryFile> filesToIgnore)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
