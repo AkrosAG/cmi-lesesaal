ae17f0deaac386324ff949cd00b828f53326b356
Jörg Lang <jlang@evelix.ch>
2024-02-07 09:23:12 +0100

    Anpassungen Code Review

--- CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
+++ CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
@@ -8,47 +8,55 @@ using System.Text;
 
 namespace CMI.Access.Repository.Systems.Rosetta;
 
-public class RosettaDataAccess: IRosettaDataAccess
+public class RosettaDataAccess : IRosettaDataAccess
 {
     private readonly string password = Settings.Default.RepositoryPassword;
     private readonly string serviceUrl = Settings.Default.RepositoryServiceUrl;
     private readonly string username = Settings.Default.RepositoryUser;
-    private readonly string exportOption = Settings.Default.RepositoryExportOption;
 
 
     public Task<string> ExportIntellectualEntity(string entityId)
     {
-        var entityData  = string.Empty;
+        var entityData = string.Empty;
         try
         {
             // e. g.: https://app.data-archive-test.ethz.ch/rest/v0/ies/IE7731039?op=export
-            var url = serviceUrl + entityId + exportOption;
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.Method = "post";
-            httpWebRequest.PreAuthenticate = true;
-            // credentials müssen selber konvertiert werden, dass httpWebRequest.Credentials = new NetworkCredential(username, password) funktioniert nicht NetworkCredential(username, password);
-            var credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
-            httpWebRequest.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
-            var responseObj = httpWebRequest.GetResponse();
 
-            using (var stream = responseObj.GetResponseStream())
+            // Call a web rest service to export the intellectual entity
+            // Use post method to call the service and use basic authentication
+            var url = string.Format(serviceUrl, entityId);
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/xml";
+            request.Accept = "application/xml";
+            request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes($"{username}:{password}")));
+            
+            var response = (HttpWebResponse)request.GetResponse();
+            using var stream = response.GetResponseStream();
+            if (stream != null)
             {
                 var sr = new StreamReader(stream);
                 entityData = sr.ReadToEnd();
                 sr.Close();
             }
+
+            Log.Information($"Intellectual Entity {entityId} exported successfully.");
+
         }
         catch (WebException ex)
         {
             var response = (HttpWebResponse)ex.Response;
-            var responseErrorData = string.Empty;
+            string responseErrorData;
             using (var stream = response.GetResponseStream())
             {
-                var sr = new StreamReader(stream);
-                responseErrorData = sr.ReadToEnd();
-                sr.Close();
+                if (stream != null)
+                {
+                    var sr = new StreamReader(stream);
+                    responseErrorData = sr.ReadToEnd();
+                    sr.Close();
+                }
             }
-            
+
             Log.Error(ex, $"Known errors when exporting Intellectual Entity {responseErrorData}");
             throw;
         }
