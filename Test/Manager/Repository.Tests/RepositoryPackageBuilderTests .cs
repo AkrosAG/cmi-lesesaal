34484e2b20857ad40bb61e80c37aa3d8db9b51ad
Ulrich Günther <uguenther@ensocon.net>
2024-03-09 22:54:13 +0100

    Test für Builder

---
+++ Test/Manager/Repository.Tests/RepositoryPackageBuilderTests .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Threading.Tasks;
+using CMI.Access.Repository.Systems.Rosetta;
+using CMI.Contract.Common;
+using CMI.Contract.Common.Gebrauchskopie;
+using CMI.Engine.PackageMetadata.Systems.Dir;
+using CMI.Manager.Repository.Systems.Rosetta;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CMI.Manager.Repository.Tests
+{
+    [TestFixture]
+    public class RepositoryPackageBuilderTests
+    {
+        [Test]
+        public async Task Build_Repository_Package_returns_correct_item()
+        {
+            // Arrange
+            var fileshare = @"\\nas12.ethz.ch\ethbib_rosetta_test_vls_transfer_s1\vls";
+            var fileUrl = $@"{fileshare}\IE268715\ie.xml";
+            
+            // Act
+            var builder = new RepositoryPackageBuilder(null, null);
+            await builder.BuildAsync(fileUrl);
+
+            // Assert
+        }
+    }
+}
