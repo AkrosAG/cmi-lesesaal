4f58bac4fef893951521260885adc74f9616dcdb
Ulrich Günther <uguenther@ensocon.net>
2024-03-12 09:20:19 +0100

    ArchiveRecord verwenden ..

--- CMI/Manager/Repository/Systems/Rosetta/RepositoryPackageBuilder.cs
+++ CMI/Manager/Repository/Systems/Rosetta/RepositoryPackageBuilder.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
+using Autofac.Features.Metadata;
 using CMI.Access.Repository.Systems.Rosetta;
 using CMI.Contract.Common;
+using CMI.Contract.Common.Gebrauchskopie;
 using CMI.Contract.Parameter;
 using CMI.Contract.Repository;
 using MassTransit;
@@ -24,10 +30,106 @@ namespace CMI.Manager.Repository.Systems.Rosetta
             this.bus = bus;
         }
 
-        public async Task<RepositoryPackage> BuildAsync(string fileUrl)
+        public async Task<RepositoryPackage> BuildAsync(string fileUrl, ElasticArchiveRecord archiveRecord)
         {
-            var doc = XDocument.Load(fileUrl);
+            OrdnungssystempositionDIP dip;
+            var namespaceManager = new XmlNamespaceManager(new NameTable());
+            namespaceManager.AddNamespace("mets", "http://www.loc.gov/METS/");
+            
+            XDocument root;
+            try
+            {
+                root = XDocument.Load(fileUrl);
+                
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $@"Error while loading {fileUrl} file.");
+                throw;
+            }
+
+            var package = GetPackageFromXml(root, archiveRecord);
+            dip = package.Ablieferung.Ordnungssystem.Ordnungssystemposition.First();
+
+            var dossier = GetDossierFromXml(root, archiveRecord);
+            dip.Dossier.Add(dossier);
+
+            var entryStruct = root.XPathSelectElements("/mets:mets/mets:structMap/mets:div/mets:div/mets:div[@TYPE='LOGIC']", namespaceManager)
+                                  
+                                  .ToList();
+            if(entryStruct.Any() == false)
+            {
+                entryStruct = root.XPathSelectElements("/mets:mets/mets:structMap[@TYPE='PHYSICAL']", namespaceManager)
+                                  .ToList();
+            }
+
+
+            var master = entryStruct.AncestorsAndSelf();
+            // (e => e.Attributes().Any(a => a.Value == "PRESERVATION_MASTER;VIEW"));
+
             return await Task.FromResult<RepositoryPackage>(null);
+        }
+
+        private PaketDIP GetPackageFromXml(XDocument root, ElasticArchiveRecord archiveRecord)
+        {
+            var package = new PaketDIP
+            {
+                Generierungsdatum = DateTime.Now,
+                SchemaVersion = SchemaVersion.Item41,
+                Ablieferung = new AblieferungDIP()
+                {
+                    Ablieferungstyp = Ablieferungstyp.FILES, // Anhand Feld Erwerbsarten ableiten
+                    AblieferndeStelle =  "Aus Feld Akzessionen der VE, ggf. nach oben navigieren",
+                    Provenienz = new ProvenienzDIP()
+                    {
+                        AktenbildnerName = "Die ersten 200 Zeichen aus der Verwaltungsgeschichte übernehmen.",
+                    },
+                    Ordnungssystem = new OrdnungssystemDIP()
+                    {
+                        Name = "Ordnungssystem",
+                        Ordnungssystemposition = new List<OrdnungssystempositionDIP>()
+                        {
+                            new OrdnungssystempositionDIP() {Id = "1"},
+                        }
+                    }
+                }
+            };
+
+            return package;
+        }
+
+        private DossierDIP GetDossierFromXml(XDocument root, ElasticArchiveRecord archiveRecord)
+        {
+            var dossier = new DossierDIP()
+            {
+                Id = Guid.NewGuid().ToString(), // ID wäre die GUID der VE,
+                Aktenzeichen = "Verwaltungssignatur",
+                Titel = "Titel der VE oder sonst DC.title aus DmdSec",
+                Inhalt = "Form und Inhalt",
+                Erscheinungsform = ErscheinungsformDossier.digital, // Ableiten aus Überlieferungsformen
+                Umfang = "Aus Feld Umfang",
+                Entstehungszeitraum = new HistorischerZeitraum()
+                {
+                    Von = new HistorischerZeitpunkt() { Datum = "aus Entstehungszeitraum" },
+                    Bis = new HistorischerZeitpunkt() { Datum = "aus Entstehungszeitraum" }
+                },
+                EntstehungszeitraumAnmerkung = "Aus Feld Bemerkungen zur Datierung",
+                Datenschutz = false, // Ableiten aus ablauf schutzfrist
+                Oeffentlichkeitsstatus = "Feld Benutzbarkeit",
+                SonstigeBestimmungen = "Aus Feld Zugangsbestimmungen",
+                zusatzDaten = new List<ZusatzDatenMerkmal>()
+                {
+                    new ZusatzDatenMerkmal() {Name = "Signatur", Value = "Aus Feld Signatur"},
+                    new ZusatzDatenMerkmal() {Name = "Stufe", Value = "Aus Feld Stufe"},
+                    new ZusatzDatenMerkmal() {Name = "Frühere Signaturen", Value = "Aus Feld Alte Signatur"},
+                    new ZusatzDatenMerkmal()
+                        {Name = "Archivplankontext", Value = "Serialisiertes XML des Archivplankontexts"},
+                    new ZusatzDatenMerkmal()
+                        {Name = "Identifikation digitales Magazin", Value = "Nummer der IE"},
+                },
+            };
+
+            return dossier;
         }
     }
 }
