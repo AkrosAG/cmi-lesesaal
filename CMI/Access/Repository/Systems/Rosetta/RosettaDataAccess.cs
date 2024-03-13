8df05c6589871d55ba99144b693be341481976a3
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-03-13 14:39:08 +0100

    Export komplett und kopiert

--- CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
+++ CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
@@ -3,15 +3,19 @@ using CMI.Access.Repository.Properties;
 using System.IO;
 using System.Net;
 using System;
+using System.Linq;
 using Serilog;
-using System.Text;
-using System.Net.Http;
+using CMI.Access.Repository.Systems.Rosetta.Helper;
 
 namespace CMI.Access.Repository.Systems.Rosetta;
 
 public class RosettaDataAccess : IRosettaDataAccess
 {
-    private readonly string serviceUrl = Settings.Default.RepositoryServiceUrl;
+    private readonly string userName = Settings.Default.RepositoryDirectoryUser;
+    private readonly string password = Settings.Default.RepositoryDirectoryPassword;
+    private readonly string directory = Settings.Default.RepositoryDirectory;
+    private readonly string domain = Settings.Default.RepositoryDomain;
+
     private readonly RosettaConnector rosettaConnector;
 
     public RosettaDataAccess(RosettaConnector connector)
@@ -19,17 +23,57 @@ public class RosettaDataAccess : IRosettaDataAccess
         rosettaConnector = connector;
     }
 
-    public async Task<string> ExportIntellectualEntity(string entityId)
+    public async Task<bool> ExportIntellectualEntity(string defaultTempStoragePath, string entityId)
     {
         entityId = "IE444295";
-        var path = await rosettaConnector.StartExportAsync(entityId); 
-        if(!string.IsNullOrEmpty(path))
+        var success = await rosettaConnector.StartExportAsync(entityId); 
+        if(success)
         {
-            path = $@"X:\\{entityId}"; // Map this  @"\\nas12.ethz.ch\ethbib_rosetta_test_vls_transfer_s1";
-            Log.Information($"Intellectual Entity {entityId} exported successfully to {path}.");
-            return path;
+            using (new ConnectToSharedFolder(directory, new NetworkCredential(userName, password, domain)))
+            {
+                var directoryEntity = Directory.GetDirectories(directory).First(f => f.EndsWith(entityId));
+                var copyPath = Path.Combine(defaultTempStoragePath, entityId);
+                if (Directory.Exists(copyPath))
+                {
+                    Directory.Delete(copyPath, true);
+                }
+                CopyDirectory(directoryEntity, copyPath);
+                Directory.Delete(directoryEntity, true);
+            }
+            Log.Information($"Intellectual Entity {entityId} exported successfully to {Path.Combine(defaultTempStoragePath, entityId)}");
         }
         
-        return null;
+        return success;
+    }
+
+    private static void CopyDirectory(string sourceDir, string destinationDir)
+    {
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+        {
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+        }
+
+        // Cache directories before we start copying
+        var dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (var file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        foreach (DirectoryInfo subDir in dirs)
+        {
+            string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+            CopyDirectory(subDir.FullName, newDestinationDir);
+        }
     }
 }
--- CMI/Manager/Repository/Systems/Rosetta/RosettaRepositoryProvider.cs
+++ CMI/Manager/Repository/Systems/Rosetta/RosettaRepositoryProvider.cs
@@ -12,6 +12,7 @@ using CMI.Contract.Repository;
 using MassTransit;
 using Serilog;
 using System.Xml.Linq;
+using CMI.Manager.Repository.Properties;
 
 namespace CMI.Manager.Repository.Systems.Rosetta
 {
@@ -22,7 +23,6 @@ namespace CMI.Manager.Repository.Systems.Rosetta
         private readonly IParameterHelper parameterHelper;
         private readonly RepositoryPackageBuilder builder;
         private readonly IBus bus;
-     
 
         public RosettaRepositoryProvider(IRosettaDataAccess rosettaDataAccess, IPackageHandler handler,
             IParameterHelper parameterHelper, RepositoryPackageBuilder builder,  IBus bus)
@@ -50,8 +50,9 @@ namespace CMI.Manager.Repository.Systems.Rosetta
                 };
             }
 
-            var exportPath =  await rosettaDataAccess.ExportIntellectualEntity(packageId);
-            if(exportPath == null)
+            // Die IntellectualEntity ist schon exportiert
+            // var exportPath =  await rosettaDataAccess.ExportIntellectualEntity(packageId);
+            // if(exportPath == null)
             {
                 return new RepositoryPackageResult
                 {
@@ -59,46 +60,23 @@ namespace CMI.Manager.Repository.Systems.Rosetta
                     ErrorMessage = "Export has failed."
                 };
             }
-
-            return new RepositoryPackageResult
-            {
-                Success = true,
-                PackageDetails = null
-            };
         }
 
-        public async Task<RepositoryPackageInfoResult> ReadPackageMetadata(string packageId, string archiveRecordId)
+        public async Task<RepositoryPackageInfoResult> ReadPackageMetadata(ElasticArchiveRecord elasticArchiveRecord)
         {
-            packageId = "IE444295";
+            var success = await rosettaDataAccess.ExportIntellectualEntity(Settings.Default.TempStoragePath, elasticArchiveRecord.PrimaryDataLink);
 
-            var fileshare = await rosettaDataAccess.ExportIntellectualEntity(packageId);
-            var fileUrl = $@"{fileshare}\IE268715\ie.xml";
-            
-            if(!File.Exists(fileUrl))
+            success = success
+                ? builder.CreateMetadataXml($@"{Path.Combine(Settings.Default.TempStoragePath, elasticArchiveRecord.PrimaryDataLink)}",
+                    elasticArchiveRecord)
+                : false;
+
+            // ToDo var package = await builder.BuildAsync(fileUrl, response.Message.ElasticArchiveRecord);
+            return new RepositoryPackageInfoResult
             {
-                return new RepositoryPackageInfoResult
-                {
-                    Success = false,
-                    ErrorMessage = $"File {fileUrl} not found."
-                };
-            }
-
-            var requestClient = bus.CreateRequestClient<FindArchiveRecordRequest>(new Uri(bus.Address, BusConstants.IndexManagerFindArchiveRecordMessageQueue), TimeSpan.FromSeconds(10));
-            var response = await requestClient.GetResponse<FindArchiveRecordResponse>(new FindArchiveRecordRequest { ArchiveRecordId = archiveRecordId });
-
-            var package = await builder.BuildAsync(fileUrl, response.Message.ElasticArchiveRecord);
-            if(package == null)
-            {
-                return new RepositoryPackageInfoResult
-                {
-                    Success = false,
-                    ErrorMessage = "Package could not be created."
-                };
-            }
-
-            // TODO: DLS-333 Rosetta-Anbindung (Import einer IntellectualEntity)
-
-            return null;
+                Success = success,
+                Valid = success // ToDo
+            };
         }
     }
 }
