74e23c031c03c8d767eed36b8b89b653b55b1bbd
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-03-13 14:39:08 +0100

    Export komplett und kopiert

--- CMI/Access/Repository/Systems/Rosetta/IRosettaDataAccess.cs
+++ CMI/Access/Repository/Systems/Rosetta/IRosettaDataAccess.cs
@@ -1,10 +1,9 @@
-﻿using DotCMIS.Client;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace CMI.Access.Repository.Systems.Rosetta
 {
     public interface IRosettaDataAccess
     {
-        public Task<string> ExportIntellectualEntity(string entityId);
+        public Task<bool> ExportIntellectualEntity(string defaultTempStoragePath, string entityId);
     }
 }
