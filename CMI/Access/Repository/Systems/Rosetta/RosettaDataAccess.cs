477047e82d2fabe4ed286deb931b193e486314d8
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-03-15 12:00:08 +0100

    Update RosettaDataAccess.cs

--- CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
+++ CMI/Access/Repository/Systems/Rosetta/RosettaDataAccess.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading.Tasks;
-using CMI.Access.Repository.Properties;
+﻿using CMI.Access.Repository.Properties;
+using CMI.Access.Repository.Systems.Rosetta.Helper;
+
+using System.Threading.Tasks;
 using System.IO;
 using System.Net;
 using System;
+using System.IO.Compression;
 using System.Linq;
 using Serilog;
-using CMI.Access.Repository.Systems.Rosetta.Helper;
+
 
 namespace CMI.Access.Repository.Systems.Rosetta;
 
@@ -25,25 +28,53 @@ public class RosettaDataAccess : IRosettaDataAccess
 
     public async Task<bool> ExportIntellectualEntity(string defaultTempStoragePath, string entityId)
     {
+        // Test IE
         entityId = "IE444295";
         var success = await rosettaConnector.StartExportAsync(entityId); 
         if(success)
         {
             using (new ConnectToSharedFolder(directory, new NetworkCredential(userName, password, domain)))
             {
-                var directoryEntity = Directory.GetDirectories(directory).First(f => f.EndsWith(entityId));
-                var copyPath = Path.Combine(defaultTempStoragePath, entityId);
-                if (Directory.Exists(copyPath))
+                try
                 {
-                    Directory.Delete(copyPath, true);
+                    CopyAndExtractIntellectualEntity(defaultTempStoragePath, entityId);
                 }
-                CopyDirectory(directoryEntity, copyPath);
-                Directory.Delete(directoryEntity, true);
+                catch (Exception e)
+                {
+                    success = false;
+                    Log.Error(e, $"An error occurred when copying the Intellectual Entity {entityId}");
+                }
             }
             Log.Information($"Intellectual Entity {entityId} exported successfully to {Path.Combine(defaultTempStoragePath, entityId)}");
         }
         
         return success;
+    }
+
+    private void CopyAndExtractIntellectualEntity(string defaultTempStoragePath, string entityId)
+    {
+        var directoryEntity = Directory.GetDirectories(directory).First(f => f.EndsWith(entityId));
+        var copyPath = Path.Combine(defaultTempStoragePath, entityId);
+        if (Directory.Exists(copyPath))
+        {
+            Directory.Delete(copyPath, true);
+        }
+
+        CopyDirectory(directoryEntity, copyPath);
+        var dir = Directory.GetDirectories(copyPath).Length == 1
+            ? Directory.GetDirectories(copyPath).First()
+            : throw new Exception("Too many directories, one was expected");
+        var file = Directory.GetFiles(dir, "*.tar").Length == 1
+            ? Directory.GetFiles(dir, "*.tar").First()
+            : throw new Exception("Too many tar-Files, one was expected");
+        
+        var fileInfo = new FileInfo(file);
+        var newDic = Directory.CreateDirectory(Path.Combine(dir,
+            fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length)));
+
+        // ToDo: Anderes entpackungstool verwenden
+        ZipFile.ExtractToDirectory(fileInfo.FullName, newDic.FullName);
+        Directory.Delete(directoryEntity, true);
     }
 
     private static void CopyDirectory(string sourceDir, string destinationDir)
@@ -66,13 +97,13 @@ public class RosettaDataAccess : IRosettaDataAccess
         // Get the files in the source directory and copy to the destination directory
         foreach (var file in dir.GetFiles())
         {
-            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            var targetFilePath = Path.Combine(destinationDir, file.Name);
             file.CopyTo(targetFilePath);
         }
 
         foreach (DirectoryInfo subDir in dirs)
         {
-            string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+            var newDestinationDir = Path.Combine(destinationDir, subDir.Name);
             CopyDirectory(subDir.FullName, newDestinationDir);
         }
     }
