aba107808816fe2a1bd52dff4beeb08770a02a9b
Ulrich Günther <uguenther@ensocon.net>
2024-03-19 20:03:05 +0100

    review nicht nötig

--- CMI/Web.Clients/web-management/src/app/modules/ordermanagement/components/ordersListPage/ordersListPage.component.ts
+++ CMI/Web.Clients/web-management/src/app/modules/ordermanagement/components/ordersListPage/ordersListPage.component.ts
@@ -48,7 +48,6 @@ export class OrdersListPageComponent implements OnInit {
 	public showDigitalisierungAbschliessen = false;
 	public showBarCode = false;
 	public hasRight = false;
-	public hasDigitalisierungsAuftrag = false;
 
 	// @ts-ignore
 	private barcodeSet: boolean;
