0016fabcd3b096a543ffc40eb296fc2a9bf9feb8
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-04-25 13:11:35 +0200

    DLS-403 Kontrolle der Aufbereiteten Daten

--- CMI/Web.Clients/web-frontend/src/app/modules/client/components/detail/viewerSection/viewerSection.component.ts
+++ CMI/Web.Clients/web-frontend/src/app/modules/client/components/detail/viewerSection/viewerSection.component.ts
@@ -27,7 +27,6 @@ export class ViewerSectionComponent implements OnInit {
 	public ngOnInit(): void {
 		this.viewerText = this._txt.translate('Konsultieren Sie die Unterlagen', 'onlineViewer.infoText');
 		this.link = this.viewerLink + this.manifestLink;
-		console.log(this.link);
 	}
 
 	public logViewerClick() {
