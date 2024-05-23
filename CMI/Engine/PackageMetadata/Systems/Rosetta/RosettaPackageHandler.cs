7686a5a81782ddbf5213a519832e55ff96be4679
PeterEttwein-Evelix <PEttwein@evelix.ch>
2024-05-23 17:09:10 +0200

    Versuch synchron mit Dir

--- CMI/Engine/PackageMetadata/Systems/Rosetta/RosettaPackageHandler.cs
+++ CMI/Engine/PackageMetadata/Systems/Rosetta/RosettaPackageHandler.cs
@@ -1,23 +1,335 @@
 ﻿using CMI.Contract.Common;
+using CMI.Contract.Common.Gebrauchskopie;
+using CMI.Contract.Messaging;
 using CMI.Contract.Repository;
+using MassTransit;
+using Serilog;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using CMI.Engine.PackageMetadata.Systems.Rosetta.Schema;
+using Newtonsoft.Json;
+using CMI.Engine.PackageMetadata.Properties;
 using System.Threading.Tasks;
 
 namespace CMI.Engine.PackageMetadata.Systems.Rosetta
 {
     public class RosettaPackageHandler : IPackageHandler
     {
-        private readonly IRepositoryPackageBuilder builder;
-
-
-        public RosettaPackageHandler(IRepositoryPackageBuilder builder)
+        private IRequestClient<FindArchiveRecordRequest> indexClient;
+        public RosettaPackageHandler(IRequestClient<FindArchiveRecordRequest> indexClient)
         {
-            this.builder = builder;
+            this.indexClient = indexClient;
         }
 
         public async Task CreateMetadataXml(string folderName, RepositoryPackage package, List<RepositoryFile> filesToIgnore)
         {
-            await builder.CreateMetadataXml(package);
+            var archiveRecord = indexClient.GetResponse<FindArchiveRecordResponse>(new FindArchiveRecordRequest { ArchiveRecordId = package.ArchiveRecordId }).Result.Message.ElasticArchiveRecord;
+            var pickupDirectory = Path.Combine(folderName, package.ArchiveRecordId);
+            if (!Directory.Exists(pickupDirectory))
+            {
+                Directory.CreateDirectory(pickupDirectory);
+            }
+
+            var headerDir = Path.Combine(pickupDirectory, "header");
+            if (!Directory.Exists(headerDir))
+            {
+                Directory.CreateDirectory(headerDir);
+            }
+
+            var paket = GetPackageFromXml(archiveRecord);
+            var ordnungssystemposition = paket.Ablieferung.Ordnungssystem.Ordnungssystemposition.First();
+            var fileUrl = $@"{Path.Combine(Settings.Default.TempStoragePath, archiveRecord.PrimaryDataLink)}\ie.xml";
+            var mets = Mets.LoadFromFile(fileUrl);
+
+            // Das erste DIV des Masters ist immer die "Table of Contents". Dieses DIV ist für uns das Root
+            var tableOfContent = mets.GetTableOfContent();
+
+            ordnungssystemposition.Dossier = new List<DossierDIP> { GetDossierFromElastic(archiveRecord) };
+            var dossierDip = ordnungssystemposition.Dossier.First();
+            // Jetzt lesen wir Rekursiv die Dateien und Unterordner aus und fügen diese dem Dossier hinzu
+            AddSubdossiers(dossierDip, tableOfContent, mets);
+
+            var contentRoot = new OrdnerDIP
+            {
+                Id = $"contentRoot{DateTime.Now.Ticks}",
+                Name = "content",
+                OriginalName = "content"
+            };
+
+            paket.Inhaltsverzeichnis.Ordner.Add(contentRoot);
+            var folder = mets.GetImportFolderName();
+
+            var sourcePath = Path.Combine(Path.GetDirectoryName(fileUrl), folder);
+            Log.Information($"Package Source Path: {sourcePath}");
+            AddInhaltsverzeichnis(contentRoot, sourcePath, mets);
+            var metadataXmlPath = Path.Combine(headerDir, "metadata.xml");
+            ((Paket)paket).SaveToFile(metadataXmlPath);
+        }
+        
+        private void AddInhaltsverzeichnis(OrdnerDIP ordner, string rootPath, Mets mets)
+        {
+            var subdirs = Directory.GetDirectories(rootPath);
+            // Get the files in the repository directory and copy to the local directory
+            foreach (var file in Directory.GetFiles(rootPath))
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Exists)
+                {
+                    var datei = new DateiDIP();
+                    if (fileInfo.Name.Contains('_'))
+                    {
+                        var id = fileInfo.Name.Substring(0, fileInfo.Name.IndexOf('_'));
+                        datei.Id = id;
+                        datei.Name = fileInfo.Name;
+                        datei.Pruefalgorithmus = DateiPruefalgorithmus(mets, id);
+                        datei.OriginalName = GetTechnicalMetadataForFile(id, Section.GeneralFileCharacteristics,
+                                SectionGeneralFileCharacteristics.FileOriginalName, mets);
+                        datei.Pruefsumme = GetTechnicalMetadataForFile(id, Section.FileFixity, SectionFileFixity.FixityValue, mets);
+
+                        var sizeInBytes = GetTechnicalMetadataForFile(id, Section.GeneralFileCharacteristics,
+                            SectionGeneralFileCharacteristics.FileSizeBytes, mets);
+                        datei.Eigenschaft = new List<EigenschaftDatei>
+                        {
+                            new ()
+                            {
+                                Value = sizeInBytes,
+                                Name = "FileSizeBytes"
+                            },
+                            new()
+                            {
+                                Value = GetTechnicalMetadataForFile(id, Section.GeneralFileCharacteristics,
+                                    SectionGeneralFileCharacteristics.FormatLibraryId, mets),
+                                Name = "FormatLibraryId"
+                            },
+                            new ()
+                            {
+                                Value = GetTechnicalMetadataForFile(id, Section.GeneralFileCharacteristics,
+                                    SectionGeneralFileCharacteristics.FileModificationDate, mets),
+                                Name = "FileModificationDate"
+                            }
+                        };
+                    }
+                    else
+                    {
+                        datei.Id = fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length);
+                        datei.Name = fileInfo.Name;
+                    }
+
+                    ordner.Datei.Add(datei);
+                }
+            }
+
+            foreach (var subDir in subdirs)
+            {
+                var subOrdner = new OrdnerDIP
+                {
+                    Id = subDir,
+                    Name = subDir
+                };
+                AddInhaltsverzeichnis(subOrdner, subDir, mets);
+                ordner.Ordner.Add(subOrdner);
+
+            }
+        }
+
+        private static Pruefalgorithmus DateiPruefalgorithmus(Mets mets, string id)
+        {
+            switch (GetTechnicalMetadataForFile(id, Section.FileFixity, SectionFileFixity.FixityType, mets))
+            {
+                case "MD5":
+                    return Pruefalgorithmus.MD5;
+                case "SHA1":
+                    return Pruefalgorithmus.SHA1;
+                case "SHA256":
+                    return Pruefalgorithmus.SHA256;
+                case "SHA512":
+                    return Pruefalgorithmus.SHA512;
+                default:
+                    return Pruefalgorithmus.MD5;
+
+            }
+        }
+        
+        private DossierDIP GetDossierFromElastic(ElasticArchiveRecord archiveRecord)
+        {
+            var dossier = new DossierDIP
+            {
+                Id = archiveRecord.ArchiveRecordId,
+                Aktenzeichen = archiveRecord.DetailData.Any(d => d.ElementName == "Verwaltungssignatur") ? string.Join(",", archiveRecord.DetailData?.FirstOrDefault(d => d.ElementName == "Verwaltungssignatur")?.TextValues.ToArray()) : string.Empty,
+                Titel = archiveRecord.Title,
+                Inhalt = archiveRecord.Contains,
+                Erscheinungsform = archiveRecord.DetailData.Any(d => d.ElementName == "Ueberlieferungsform") ?
+                    archiveRecord.DetailData?.FirstOrDefault(d => d.ElementName == "Ueberlieferungsform")?.TextValues.FirstOrDefault().ToLower() == "analog" ?
+                    ErscheinungsformDossier.nichtdigital : ErscheinungsformDossier.digital : ErscheinungsformDossier.keineAngabe,
+                Umfang = archiveRecord.DetailData.Any(dd => dd.ElementName.Equals("Umfang"))
+                    ? string.Join(",", archiveRecord.DetailData.First(dd => dd.ElementName.Equals("Umfang")).TextValues)
+                    : string.Empty,
+                Entstehungszeitraum = GetEntstehungszeitraum(archiveRecord.CreationPeriod),
+                EntstehungszeitraumAnmerkung = archiveRecord.DetailData.Any(dd => dd.ElementName.Equals("BemerkungDatierung"))
+                    ? string.Join(",", archiveRecord.DetailData.First(dd => dd.ElementName.Equals("BemerkungDatierung")).TextValues)
+                    : string.Empty,
+                Datenschutz = archiveRecord.ProtectionEndDate?.Date <= DateTime.Now, // Ableiten aus ablauf schutzfrist
+                Oeffentlichkeitsstatus = archiveRecord.Permission,
+                SonstigeBestimmungen = archiveRecord.Accessibility,
+                zusatzDaten = new List<ZusatzDatenMerkmal>
+                {
+                    new() { Name = "Signatur", Value = archiveRecord.ReferenceCode },
+                    new() { Name = "Stufe", Value = archiveRecord.Level },
+                    new() { Name = "Frühere Signaturen", Value = archiveRecord.FormerReferenceCode },
+                    new() { Name = "Archivplankontext", Value = JsonConvert.SerializeObject(archiveRecord.ArchiveplanContext) },
+                    new() { Name = "Identifikation digitales Magazin", Value = archiveRecord.PrimaryDataLink }
+                }
+            };
+
+            return dossier;
+        }
+
+        private static void AddSubdossiers(DossierDIP dossier, DivType root, Mets mets)
+        {
+            foreach (var div in root.Div)
+            {
+                // Verarbeite Nodes vom Typ File
+                if (div.IsFileNode())
+                {
+                    ProcessFileNode(dossier, div, mets);
+                }
+
+                // Verarbeite Nodes vom Typ Verzeichniss or Null Type nodes welche als Verzeichnisse interpretiert werden
+                else if (div.IsFolderNode() || div.IsEmptyTypeNode())
+                {
+                    ProcessFolderOrEmptyNode(dossier, div, mets);
+                }
+            }
+        }
+
+        private static void ProcessFileNode(DossierDIP dossier, DivType div, Mets mets)
+        {
+            var firstFptr = div.Fptr.First();
+            var dokument = new DokumentDIP()
+            {
+                Id = string.IsNullOrEmpty(firstFptr.ID) ? firstFptr.FILEID : firstFptr.ID,
+                Titel = GetTechnicalMetadataForFile(firstFptr.FILEID, Section.GeneralFileCharacteristics, SectionGeneralFileCharacteristics.Label, mets),
+                Erscheinungsform = ErscheinungsformDokument.digital,
+                // Bei Rosetta gibt es vermutlich immer nur einen FilePointer, aber theoretisch könnte es auch mehr sein
+                DateiRef = div.Fptr.Select(fptr => fptr.FILEID).ToList()
+            };
+
+            dossier.Dokument.Add(dokument);
+        }
+
+        private static void ProcessFolderOrEmptyNode(DossierDIP dossier, DivType div, Mets mets)
+        {
+            var subDossier = new DossierDIP()
+            {
+                Id = div.ID ?? div.LABEL,
+                DateiRef = new List<string>(),
+                Titel = div.LABEL,
+                Entstehungszeitraum = dossier.Entstehungszeitraum
+            };
+
+            if (div.HasSubNodes())
+            {
+                AddSubdossiers(subDossier, div, mets);
+                dossier.Dossier.Add(subDossier);
+            }
+        }
+        
+        private PaketDIP GetPackageFromXml(ElasticArchiveRecord archiveRecord)
+        {
+            var package = new PaketDIP
+            {
+                Generierungsdatum = DateTime.Now,
+                SchemaVersion = SchemaVersion.Item41,
+                Ablieferung = new AblieferungDIP
+                {
+                    Ablieferungstyp = Ablieferungstyp.FILES, // Anhand Feld Erwerbsarten ableiten
+                    AblieferndeStelle = archiveRecord?.DetailData?.FirstOrDefault(d => d.ElementName.Equals("AbgebendeStelle", StringComparison.InvariantCultureIgnoreCase))?.TextValues?.First(),
+                    Provenienz = new ProvenienzDIP
+                    {
+                        AktenbildnerName = archiveRecord?.AdministrativeHistory?.Substring(0, 200) // "Die ersten 200 Zeichen aus der Verwaltungsgeschichte übernehmen.",
+                    },
+                    Ordnungssystem = new OrdnungssystemDIP()
+                    {
+                        Name = "Ordnungssystem",
+                        Ordnungssystemposition = new List<OrdnungssystempositionDIP>
+                        {
+                            new () {Id = "1"}
+                        }
+                    }
+                }
+            };
+
+            return package;
+        }
+        
+        private static string GetTechnicalMetadataForFile(string fileId, string sectionName, string keyId, Mets mets)
+        {
+            try
+            {
+                var amdSecId = GetAmdSecIdFromFileSec(fileId, mets.FileSec.FileGrp.Cast<object>().ToList());
+
+                var amdSec = mets.AmdSec.Find(a => a.ID.Equals(amdSecId));
+
+                if (amdSec.TechMD[0].MdWrap.Item is MdSecTypeMdWrapXmlData technischeMetadatenWrapper)
+                {
+                    var xmlDoc = XDocument.Parse(technischeMetadatenWrapper.Any[0].OuterXml);
+                    var element = xmlDoc.Descendants().FirstOrDefault(n =>
+                        n.Attributes().Any(a => a.Name.LocalName.Equals("id") && a.Value.Equals(sectionName)));
+                    if (element != null)
+                    {
+                        var value = element.Descendants().FirstOrDefault(n => n.Name.LocalName.Equals("key") &&
+                                                                              n.Attributes().Any(a => a.Name.LocalName.Equals("id")
+                                                                                  && a.Value.Equals(keyId)));
+                        return value?.Value;
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e, $"Field {fileId} was in xml not found");
+                return $"Field {fileId} Not found";
+            }
+
+            // Finde File in fileSec
+            return fileId;
+        }
+
+
+        private static string GetAmdSecIdFromFileSec(string fileId, List<object> items)
+        {
+            foreach (var item in items)
+            {
+                if (item is FileType file && file.ID.Equals(fileId))
+                {
+                    return file.ADMID;
+                }
+
+                if (item is FileGrpType grp)
+                {
+                    var retVal = GetAmdSecIdFromFileSec(fileId, grp.Items);
+                    if (retVal != null)
+                    {
+                        return retVal;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private HistorischerZeitraum GetEntstehungszeitraum(ElasticTimePeriod creationPeriod)
+        {
+            var retVal = new HistorischerZeitraum
+            {
+                Von = new HistorischerZeitpunkt
+                    { Ca = creationPeriod.StartDateApproxIndicator, Datum = creationPeriod.StartDate.ToString("yyyy-MM-dd") },
+                Bis = new HistorischerZeitpunkt { Ca = creationPeriod.EndDateApproxIndicator, Datum = creationPeriod.EndDate.ToString("yyyy-MM-dd") }
+            };
+            return retVal;
         }
     }
 
--- CMI/Manager/Repository/Systems/Rosetta/RosettaRepositoryProvider.cs
+++ CMI/Manager/Repository/Systems/Rosetta/RosettaRepositoryProvider.cs
@@ -8,23 +8,27 @@ using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using CMI.Engine.PackageMetadata.Systems.Rosetta;
 using System.Diagnostics;
+using CMI.Engine.PackageMetadata.Systems.Rosetta.Schema;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using File = System.IO.File;
 
 namespace CMI.Manager.Repository.Systems.Rosetta
 {
     public class RosettaRepositoryProvider : IRepositoryProvider
     {
         private readonly IRosettaDataAccess rosettaDataAccess;
-        private readonly IRepositoryPackageBuilder builder;
+        private readonly IRequestClient<FindArchiveRecordRequest> indexClient;
         private readonly IBus bus;
         private readonly IPackageHandler handler;
 
-        public RosettaRepositoryProvider(IRosettaDataAccess rosettaDataAccess, IPackageHandler handler, IRepositoryPackageBuilder builder, IBus bus)
+        public RosettaRepositoryProvider(IRosettaDataAccess rosettaDataAccess, IPackageHandler handler, IRequestClient<FindArchiveRecordRequest> indexClient, IBus bus)
         {
             this.handler = handler;
             this.rosettaDataAccess = rosettaDataAccess;       
-            this.builder = builder;                              
+            this.indexClient = indexClient;                              
             this.bus = bus;
         }
 
@@ -58,12 +62,12 @@ namespace CMI.Manager.Repository.Systems.Rosetta
             {
                 currentStatus = AufbereitungsStatusEnum.PrimaerdatenExtrahiert;
                 await UpdatePrimaerdatenAuftragStatus(primaerdatenAuftragId, currentStatus);
-
-                var repositoryPackage = await builder.BuildRepositoryPackageAsync(archiveRecordId, packageId);
+                var archiveRecord = indexClient.GetResponse<FindArchiveRecordResponse>(new FindArchiveRecordRequest { ArchiveRecordId = archiveRecordId }).Result.Message.ElasticArchiveRecord;
+                var repositoryPackage = await BuildRepositoryPackageAsync(archiveRecord);
                 // ToDo: fileTypesToIgnore
                 if (createMetadataXml)
                 {
-                    await handler.CreateMetadataXml("Braucht man bei Rosetta nicht", repositoryPackage, new List<RepositoryFile>());
+                    await handler.CreateMetadataXml(Settings.Default.FileCopyDestinationPath, repositoryPackage, new List<RepositoryFile>());
                 }
                 watch.Stop();
                 repositoryPackage.RepositoryExtractionDuration = watch.ElapsedMilliseconds;
@@ -71,7 +75,7 @@ namespace CMI.Manager.Repository.Systems.Rosetta
                 repositoryPackageResult.PackageDetails = repositoryPackage;
                 repositoryPackageResult.Valid = true;
 
-                builder.BuildZipFile(archiveRecordId, packageId);
+                BuildZipFile(archiveRecordId, packageId);
                 currentStatus = AufbereitungsStatusEnum.ZipDateiErzeugt;
                 await UpdatePrimaerdatenAuftragStatus(primaerdatenAuftragId, currentStatus);
             }
@@ -85,7 +89,7 @@ namespace CMI.Manager.Repository.Systems.Rosetta
             var success = await rosettaDataAccess.ExportIntellectualEntity(Settings.Default.TempStoragePath, elasticArchiveRecord.PrimaryDataLink);
             if (success)
             {
-                var package = await builder.BuildRepositoryPackageAsync(elasticArchiveRecord.ArchiveRecordId, elasticArchiveRecord.PrimaryDataLink);
+                var package = await BuildRepositoryPackageAsync(elasticArchiveRecord);
                 return new RepositoryPackageInfoResult
                 {
                     Success = true,
@@ -101,7 +105,118 @@ namespace CMI.Manager.Repository.Systems.Rosetta
             };
             
         }
+        
+        public async Task<RepositoryPackage> BuildRepositoryPackageAsync(ElasticArchiveRecord archiveRecord)
+        {
+            
+            var files = new List<RepositoryFile>();
+            var rootFolder = new List<RepositoryFolder>();
 
+            var fileUrl = $@"{Path.Combine(Settings.Default.TempStoragePath, archiveRecord.PrimaryDataLink)}\ie.xml";
+            var mets = Mets.LoadFromFile(fileUrl);
+
+            var folder = mets.GetImportFolderName();
+
+            var sourcePath = Path.Combine(Path.GetDirectoryName(fileUrl), folder);
+            Log.Information($"Package Source Path: {sourcePath}");
+            var totalFileSize = 0L;
+            CalculateFolderAndFileStructures(sourcePath,  rootFolder, files, ref totalFileSize);
+
+            var preZip = DateTime.Now;
+            var result = new RepositoryPackage
+            {
+                PackageFileName = archiveRecord.ArchiveRecordId + ".zip",
+                PackageId = archiveRecord.PrimaryDataLink,
+                ArchiveRecordId = archiveRecord.ArchiveRecordId,
+                SizeInBytes = totalFileSize,
+                FileCount = files.Count,
+                RepositoryExtractionDuration = DateTime.Now.Ticks - preZip.Ticks,
+                FulltextExtractionDuration = 0,
+                Files = files,
+                Folders = rootFolder
+            };
+
+            return await Task.FromResult(result);
+        }
+
+
+        private void CalculateFolderAndFileStructures(string rootPath, List<RepositoryFolder> folders, List<RepositoryFile> files, ref long totalFileSize)
+        {
+            var subFolders = new List<RepositoryFolder>();
+            var subdirs = Directory.GetDirectories(rootPath);
+            var subFiles = new List<RepositoryFile>();
+            // Get the files in the repository directory and copy to the local directory
+            foreach (var file in Directory.GetFiles(rootPath))
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Exists)
+                {
+                    totalFileSize += fileInfo.Length;
+                    var id = fileInfo.Name.Contains('_')
+                        ? fileInfo.Name.Substring(0, fileInfo.Name.IndexOf('_'))
+                        : fileInfo.Name.Substring(0, fileInfo.Name.Length - fileInfo.Extension.Length);
+                    
+                    subFiles.Add(new RepositoryFile
+                    {
+                        PhysicalName = fileInfo.FullName,
+                        Exported = true,
+                        MimeType = fileInfo.Extension,
+                        Id = id
+                    });
+                }
+            }
+
+            files.AddRange(subFiles);
+
+            foreach (var subDir in subdirs)
+            {
+
+                subFolders.Add(new RepositoryFolder()
+                {
+                    LogicalName = subDir,
+                    PhysicalName = subDir,
+                    Id = subDir
+                });
+                CalculateFolderAndFileStructures(subDir, subFolders, files, ref totalFileSize);
+            }
+
+            if (subFolders.Count > 0)
+            {
+                folders.Add(new RepositoryFolder
+                {
+                    Files = subFiles,
+                    Folders = subFolders
+                });
+            }
+        }
+       
+        private void BuildZipFile(string archiveRecordId, string primaryDataLink)
+        {
+            var fileUrl = $@"{Path.Combine(Settings.Default.TempStoragePath, primaryDataLink)}\ie.xml";
+            var mets = Mets.LoadFromFile(fileUrl);
+
+            var folder = mets.GetImportFolderName();
+
+            var sourcePath = Path.Combine(Path.GetDirectoryName(fileUrl), folder);
+            var targetFile = Path.Combine(Settings.Default.FileCopyDestinationPath, archiveRecordId + ".zip");
+            var zipBaseDir = Path.Combine(Settings.Default.FileCopyDestinationPath, archiveRecordId);
+
+            var contentDir = Path.Combine(zipBaseDir, "content");
+            var headerDir = Path.Combine(zipBaseDir, "header");
+            Directory.CreateDirectory(headerDir);
+
+            RosettaDataAccess.CopyDirectory(sourcePath, contentDir);
+
+            if (File.Exists(targetFile))
+            {
+                File.Delete(targetFile);
+            }
+
+            ZipFile.CreateFromDirectory(zipBaseDir, targetFile);
+            Log.Information("Created zip file {0}. ArchiveRecord Id: {1}", targetFile, archiveRecordId);
+            Directory.Delete(zipBaseDir, true);
+        }
+        
         private async Task UpdatePrimaerdatenAuftragStatus(int primaerdatenAuftragId, AufbereitungsStatusEnum status, string errorText = null)
         {
             if (primaerdatenAuftragId > 0)
