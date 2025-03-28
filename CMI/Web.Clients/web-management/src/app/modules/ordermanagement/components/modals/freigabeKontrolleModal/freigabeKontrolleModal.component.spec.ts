5a59873703e67f5779876a6ee8600fd354d37eb4
PeterEttwein-Evelix <PEttwein@evelix.ch>
2025-03-28 16:05:34 +0100

    Fix Unittest

--- CMI/Web.Clients/web-management/src/app/modules/ordermanagement/components/modals/freigabeKontrolleModal/freigabeKontrolleModal.component.spec.ts
+++ CMI/Web.Clients/web-management/src/app/modules/ordermanagement/components/modals/freigabeKontrolleModal/freigabeKontrolleModal.component.spec.ts
@@ -140,7 +140,7 @@ describe('FreigabeKontrolleModalPage', () => {
 
 		it('test if the date is converted correctly'
 			,() => {
-				expect(sut.datumBewilligung).toEqual(new Date('2025-08-26'));
+				expect(sut.datumBewilligung).toEqual(new Date('2025-08-26').toDateString('yyyy-mm-dd'));
 			});
 
 		it('when user is Ö3 and ApproveStatus is FreigegebenInSchutzfris ' +
