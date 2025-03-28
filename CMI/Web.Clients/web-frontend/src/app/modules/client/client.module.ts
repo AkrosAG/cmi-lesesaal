13479f63546a94244db48abbc0a8e79bd853ddf9
PeterEttwein-Evelix <PEttwein@evelix.ch>
2025-03-28 10:32:02 +0100

    Abschluss P-C Angular 18 testing

--- CMI/Web.Clients/web-frontend/src/app/modules/client/client.module.ts
+++ CMI/Web.Clients/web-frontend/src/app/modules/client/client.module.ts
@@ -2,7 +2,7 @@ import {NgModule, ModuleWithProviders } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {HttpClientModule, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {ALL_COMPONENTS} from './components/_all';
 import {ALL_DIRECTIVES} from './directives/_all';
 import {ALL_PIPES} from './pipes/_all';
@@ -12,31 +12,47 @@ import {ALL_RESOLVERS} from './routing/_all';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule, WijmoModule} from '@cmi/lesesaal-web-core';
 
-@NgModule({ declarations: [
-        ...ALL_COMPONENTS,
-        ...ALL_DIRECTIVES,
-        ...ALL_PIPES
-    ],
-    exports: [
-        ...ALL_COMPONENTS,
-        ...ALL_DIRECTIVES,
-        ...ALL_PIPES,
-        CommonModule,
-        RouterModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        WijmoModule
-    ], imports: [CommonModule,
-        FormsModule,
-        RouterModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        CoreModule,
-        WijmoModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+import {FlatpickrModule} from 'angularx-flatpickr';
+
+@NgModule({
+
+	declarations: [
+		...ALL_COMPONENTS,
+		...ALL_DIRECTIVES,
+		...ALL_PIPES
+	],
+	exports: [
+		...ALL_COMPONENTS,
+		...ALL_DIRECTIVES,
+		...ALL_PIPES,
+		CommonModule,
+		RouterModule,
+		FormsModule,
+		BrowserAnimationsModule,
+		WijmoModule
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		RouterModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		CoreModule,
+		WijmoModule,
+		/* eslint-disable */
+		FlatpickrModule.forRoot({
+			prevArrow: "<svg version='1.1\' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z'></path></svg>",
+			nextArrow: "<svg version='1.1\' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z'></path></svg>"
+		})
+	],
+	providers: [provideHttpClient(withInterceptorsFromDi())]
+
+})
 
 export class ClientModule {
 
-	public static forRoot(): ModuleWithProviders<ClientModule> {
+	public static forRoot(): ModuleWithProviders<ClientModule>  {
 		return {
 			ngModule: ClientModule,
 			providers: [
@@ -47,3 +63,4 @@ export class ClientModule {
 		};
 	}
 }
+
