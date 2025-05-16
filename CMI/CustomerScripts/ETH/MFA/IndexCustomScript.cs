4c4dd8422c7a95cafa4845c5c0f149a1f3f1eb27
PeterEttwein-Evelix <PEttwein@evelix.ch>
2025-05-16 15:41:59 +0200

    DLS-480-26-Digitale-Daten-vorhanden

--- CMI/CustomerScripts/ETH/MFA/IndexCustomScript.cs
+++ CMI/CustomerScripts/ETH/MFA/IndexCustomScript.cs
@@ -54,6 +54,18 @@ namespace CMI.Contract.Common.Compiler
                 }
             }
 
+            var digitalvorhanden = elasticArchiveRecord.DetailData.FirstOrDefault(d => d.ElementName.Equals("LinkAufDigitalesOriginal"));
+
+            elasticArchiveRecord.Facetten.Boolean01 = new List<bool>();
+            if (elasticArchiveRecord.Files != null && elasticArchiveRecord.Files.Count > 0)
+            {
+                elasticArchiveRecord.Facetten.Boolean01.Add(true);
+            }
+            else
+            {
+                elasticArchiveRecord.Facetten.Boolean01.Add(false);
+            }
+
             if (elasticArchiveRecord.Descriptors.Count > 0)
             {
                 if (elasticArchiveRecord.Descriptors.Any(d => d.Thesaurus == "Körperschaftsregister" || d.Thesaurus == "Koerperschaftsregister"))
