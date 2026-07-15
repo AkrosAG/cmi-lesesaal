9f82a6358a64de3b334862a1ff61ae5c951c0a39
PeterEttwein-Evelix <PEttwein@evelix.ch>
2026-07-15 16:50:02 +0200

    Update OrderController.cs

--- CMI/Web/CMI.Web.Frontend/api/Controllers/OrderController.cs
+++ CMI/Web/CMI.Web.Frontend/api/Controllers/OrderController.cs
@@ -293,7 +293,7 @@ namespace CMI.Web.Frontend.api.Controllers
                             userAccess, basket, orderItemIdsToExclude);
                         break;
                     case OrderType.Lesesaalausleihen:
-                        var vorlaufzeit = managementClientSettings.Vorlaufzeit;
+                        var vorlaufzeit =  GetCurrentUserAccess().RolePublicClient == AccessRoles.RoleAMA ? 0 : managementClientSettings.Vorlaufzeit;
                         leseSaalDateAsDateTime = orderParams.LesesaalDate.ParseDateTimeSwiss();
                         ValidateLesesaalBestellung(leseSaalDateAsDateTime);
                         if (DateTime.Today.AddDays(vorlaufzeit) > leseSaalDateAsDateTime.Value)
